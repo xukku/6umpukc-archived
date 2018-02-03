7d791f914779d6b34f25afc7aee874c549ceff98
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2018-02-03 19:49:03 +0300

    fix

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php require $_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php";
+$APPLICATION->SetTitle("Hello world.");
+?>
+
+<p>
+    Hello world.
+</p>
+
+<?php require $_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"; ?>
