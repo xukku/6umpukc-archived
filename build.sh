3342007d03d6daa05e257b93042412b54e7fd159
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2018-02-03 21:49:15 +0300

    fix

--- build.sh
+++ build.sh
@@ -6,10 +6,11 @@ cd tmp
 curl -OL http://www.1c-bitrix.ru/download/first_site_encode_php5.tar.gz
 
 tar -xvzf first_site_encode_php5.tar.gz \
-    --exclude "./bitrix/modules/fileman" \
     --exclude "./bitrix/modules/iblock"
 rm first_site_encode_php5.tar.gz
 
 tar -zcvf ../bitrix.tar.gz ./
 
 cd ..
+
+#    --exclude "./bitrix/modules/fileman" \
