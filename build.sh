9524f0e1ed88e76b7cbe4399ff557ac3b92fdb41
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2018-02-13 22:37:08 +0400

    add minimal

--- build.sh
+++ build.sh
@@ -5,13 +5,15 @@ cd tmp
 
 curl -OL http://www.1c-bitrix.ru/download/first_site_encode_php5.tar.gz
 
+# small
 tar -xvzf first_site_encode_php5.tar.gz \
     --exclude "./bitrix/modules/iblock"
 rm first_site_encode_php5.tar.gz
+tar -zcvf ../small.tar.gz ./
 
-tar -zcvf ../bitrix.tar.gz ./
+# minimal
+rm -R ./bitrix/modules/fileman
+rm -R ./bitrix/wizards/bitrix/first_site
+tar -zcvf ../minimal.tar.gz ./
 
 cd ..
-
-#    --exclude "./bitrix/modules/fileman" \
-#    --exclude "./bitrix/wizards/bitrix/first_site" \
