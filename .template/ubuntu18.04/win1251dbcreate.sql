68e441ce7499713f8cfd8ffaf5b9fc88a44e67b9
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2020-01-27 20:27:34 +0300

    #18

---
+++ .template/ubuntu18.04/win1251dbcreate.sql
@@ -0,0 +1,4 @@
+
+CREATE DATABASE IF NOT EXISTS bitrixdb1 CHARACTER SET cp1251 COLLATE cp1251_general_ci;
+
+GRANT ALL PRIVILEGES ON bitrixdb1.* TO bitrixuser1@localhost IDENTIFIED BY "bitrixpassword1" WITH GRANT OPTION;
