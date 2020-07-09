f8f89e1868a780b742a18d8ea37ee18ec2e0f317
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2020-07-09 23:07:32 +0300

    fix-for-bitrix-utf

--- .template/ubuntu18.04/utf8dbcreate.sql
+++ .template/ubuntu18.04/utf8dbcreate.sql
@@ -1,4 +1,4 @@
 
-CREATE DATABASE IF NOT EXISTS bitrixdb1 CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS bitrixdb1 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 GRANT ALL PRIVILEGES ON bitrixdb1.* TO bitrixuser1@localhost IDENTIFIED BY "bitrixpassword1" WITH GRANT OPTION;
