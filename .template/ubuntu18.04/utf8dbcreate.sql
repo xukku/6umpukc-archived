698579a52e60c3da286c524eb07092ee03ba72a2
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2020-07-10 12:28:03 +0300

    restore-fix-new-version-db

--- .template/ubuntu18.04/utf8dbcreate.sql
+++ .template/ubuntu18.04/utf8dbcreate.sql
@@ -1,4 +1,5 @@
 
 CREATE DATABASE IF NOT EXISTS bitrixdb1 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
-GRANT ALL PRIVILEGES ON bitrixdb1.* TO bitrixuser1@localhost IDENTIFIED BY "bitrixpassword1" WITH GRANT OPTION;
+CREATE USER bitrixuser1@localhost IDENTIFIED BY "bitrixpassword1";
+GRANT ALL PRIVILEGES ON bitrixdb1.* TO bitrixuser1@localhost WITH GRANT OPTION;
--- .template/ubuntu18.04/win1251dbcreate.sql
+++ .template/ubuntu18.04/win1251dbcreate.sql
@@ -1,4 +1,5 @@
 
 CREATE DATABASE IF NOT EXISTS bitrixdb1 CHARACTER SET cp1251 COLLATE cp1251_general_ci;
 
-GRANT ALL PRIVILEGES ON bitrixdb1.* TO bitrixuser1@localhost IDENTIFIED BY "bitrixpassword1" WITH GRANT OPTION;
+CREATE USER bitrixuser1@localhost IDENTIFIED BY "bitrixpassword1";
+GRANT ALL PRIVILEGES ON bitrixdb1.* TO bitrixuser1@localhost WITH GRANT OPTION;
