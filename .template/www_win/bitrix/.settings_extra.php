ae3556fd2c01541c52bd04e27669ca100638249a
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2021-01-17 13:38:22 +0300

    настройкм для вывода отладки

--- .template/www_win/bitrix/.settings_extra.php
+++ .template/www_win/bitrix/.settings_extra.php
@@ -5,8 +5,8 @@ return [
 		'value' => [
 			'debug' => true,
 			'handled_errors_types' => E_ALL & ~E_NOTICE & ~E_USER_NOTICE & ~E_STRICT & ~E_DEPRECATED & ~E_USER_DEPRECATED,
-			'exception_errors_types' => E_ERROR | E_WARNING | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR,
-			//'exception_errors_types' => 0,
+			//'exception_errors_types' => E_ERROR | E_WARNING | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR,
+			'exception_errors_types' => 0,
 			'ignore_silence' => false,
 			'assertion_throws_exception' => true,
 			'assertion_error_type' => 256,
