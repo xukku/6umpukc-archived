ca50550deff0f6bdcd1e9e3b5c12e7bfeaebdfde
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2021-01-17 18:03:34 +0300

    bx mod-update refresh - для перезаписи текущей версии

--- .action_mod_update.php
+++ .action_mod_update.php
@@ -1,13 +1,18 @@
 <?php
 
+$refreshVersion = !empty($_SERVER['argv'][2]) && ($_SERVER['argv'][2] == 'refresh');
+
 $basePath = getcwd();
 $fname = $basePath . '/install/version.php';
 require $fname;
 
 $arModuleVersion['VERSION_DATE'] = date('Y-m-d H:i:s');
 $version = explode('.', trim($arModuleVersion['VERSION']));
-$v = (int)array_pop($version) + 1;
-$version[] = $v;
+if (!$refreshVersion)
+{
+	$v = (int)array_pop($version) + 1;
+	$version[] = $v;
+}
 $strVersion = implode('.', $version);
 $arModuleVersion['VERSION'] = $strVersion;
 
@@ -17,6 +22,11 @@ $arModuleVersion = array(
 	"VERSION_DATE" => "' . $arModuleVersion['VERSION_DATE'] . '",
 );');
 
+if ($refreshVersion)
+{
+	system('git tag -d ' . $strVersion);
+	system('git push origin master :' . $strVersion);
+}
 system('git commit -am "Номер версии"');
 system('git tag ' . $strVersion);
 system('git push origin ' . $strVersion);
